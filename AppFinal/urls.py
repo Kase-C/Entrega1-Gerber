b473cdf27ff19726495918ddbe393810102f4d1c
Kiss-shot8 <fuchimanico@gmail.com>
2022-08-29 23:13:21 -0300

    Creacion Templates con los Statics y views basicas

--- AppFinal/urls.py
+++ AppFinal/urls.py
@@ -3,5 +3,11 @@ from .views import *
 
 
 urlpatterns = [
-
+    path('formularioUsuario/', formularioUsuario, name='formularioUsuario'),
+    path('formularioMascota/', formularioMascota, name='formularioMascota'),
+    path('formularioEstudios/', formularioEstudios, name='formularioEstudios'),
+    path('buscarUsuario/', buscarUsuario, name='buscarUsuario'),
+    path('buscarMascota/', buscarMascota, name='buscarMascota'),
+    path('buscarEstudios/', buscarEstudios, name='buscarEstudios'),
+    path('', inicio, name='inicio'),
 ]
--- AppFinal/views.py
+++ AppFinal/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+def inicio(request):
+    return render (request, "inicio.html")
+
+def formularioUsuario(request):
+    return render(request, "formularioUsuario.html")
+
+def formularioMascota(request):
+    return render(request, "formularioMascota.html")
+
+def formularioEstudios(request):
+    return render(request, "formularioEstudios.html")
+
+def buscarUsuario(request):
+    return render(request, "buscarUsuario.html")
+
+def buscarMascota(request):
+    return render(request, "buscarMascota.html")
+
+def buscarEstudios(request):
+    return render(request, "buscarEstudios.html")
